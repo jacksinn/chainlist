3b9a66b97714a2fbf15108f641bb773f42d2645d
Steven <steven@rootedlogics.com>
2018-03-08 15:37:03 -0500

    testing selling an article

--- test/ChainListHappyPath.js
+++ test/ChainListHappyPath.js
@@ -2,6 +2,12 @@ var ChainList = artifacts.require("./ChainList.sol");
 
 // Test Suite
 contract('ChainList', function(accounts){
+    var chainListInstance;
+    var seller = accounts[1];
+    var articleName = "Article 1";
+    var articleDescription = "Description for Article 1";
+    var articlePrice = 10;
+
     it("should be initialized with empty values", function(){
         return ChainList.deployed().then(function(instance){
             return instance.getArticle();
@@ -12,4 +18,21 @@ contract('ChainList', function(accounts){
             assert.equal(data[3].toNumber(), 0, "Article Price must be zero");
         })
     });
+
+    it("should sell an article", function(){
+        return ChainList.deployed().then(function(instance){
+            chainListInstance = instance;
+            return chainListInstance.sellArticle(
+                articleName, 
+                articleDescription, 
+                web3.toWei(articlePrice, "ether"), { from: seller })
+            }).then(function(){
+                return chainListInstance.getArticle();
+            }).then(function(data){
+                assert.equal(data[0], seller, "Seller must be " + seller);
+                assert.equal(data[1], articleName, "Article Name must be " + articleName);
+                assert.equal(data[2], articleDescription, "Article Description must be " + articleDescription);
+                assert.equal(data[3].toNumber(), web3.toWei(articlePrice, "ether"), "Article Price must be " + web3.toWei(articlePrice, "ether"));
+            });
+        });
 });
