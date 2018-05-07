87f16e33452f10ffd1048024169e2ae4250ecb75
Steven <steven@rootedlogics.com>
2018-05-07 10:37:41 -0400

    adding in a different 'from' account to test deployment, unlocked in truffle console

--- truffle.js
+++ truffle.js
@@ -1,11 +1,18 @@
 module.exports = {
-     // See <http://truffleframework.com/docs/advanced/configuration>
-     // to customize your Truffle configuration!
-     networks: {
-          ganache: {
-               host: "localhost",
-               port: 7545,
-               network_id: "*" // Match any network id
-          }
-     }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    chainskills: {
+      host: "localhost",
+      port: 8545,
+      network_id: "4224",
+      gas: 4700000,
+      from: '0x433e1b5c05ee4483c5b2c637dcadd607770ffb77'
+    }
+  }
 };
