0906378e5667bc70038231bdf67a4aa7e505ab30
Steven <steven@rootedlogics.com>
2018-05-09 11:28:55 -0400

    second test, making sure users can't buy their own article

--- test/ChainListExceptions.js
+++ test/ChainListExceptions.js
@@ -32,4 +32,25 @@ contract("ChainList", function(accounts){
             assert.equal(data[4].toNumber(), 0, "Article Price must be zero");        
         });
     });
+
+    // Buying an article you are selling
+    it("should throw an exception if you try to buy your own article", function(){
+        return ChainList.deployed().then(function(instance){
+            chainListInstance = instance;
+            return chainListInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {from: seller});
+        }).then(function(receipt){
+            return chainListInstance.buyArticle({from: seller, value: web3.toWei(articlePrice, "ether")});
+        }).then(assert.fail).catch(function(error){
+            assert(true);
+        }).then(function(){
+            return chainListInstance.getArticle();
+        }).then(function(data){
+            // And then check to see if these values match our pre-defined values (no data manipulation should occur)
+            assert.equal(data[0], seller, "Seller must be " + seller);
+            assert.equal(data[1], 0x0, "Buyer must be empty");
+            assert.equal(data[2], articleName, "Article Name must be " + articleName);
+            assert.equal(data[3], articleDescription, "Article Description must be " + articleDescription);
+            assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), "Article Price must be " + articlePrice);        
+        });
+    });
 });
